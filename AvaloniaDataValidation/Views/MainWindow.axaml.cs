cfc92a864d0f5debef60b7d656c5d6e3e214e09e
bodong <bodong@tencent.com>
2023-04-14 10:43:00 +0800

    let's go.

--- AvaloniaDataValidation/Views/MainWindow.axaml.cs
+++ AvaloniaDataValidation/Views/MainWindow.axaml.cs
@@ -1,4 +1,6 @@
-using Avalonia.Controls;
+﻿using Avalonia.Controls;
+using Avalonia.Interactivity;
+using System;
 
 namespace AvaloniaDataValidation.Views
 {
@@ -7,6 +9,19 @@ namespace AvaloniaDataValidation.Views
         public MainWindow()
         {
             InitializeComponent();
+        }
+
+        private void OnShowError(object sender, RoutedEventArgs e)
+        {
+            if(!DataValidationErrors.GetHasErrors(textBox_Errors))
+            {
+                DataValidationErrors.SetError(textBox_Errors, new ArgumentException("A custom exception error"));
+            }
+            else
+            {
+                DataValidationErrors.SetError(textBox_Errors, null);
+            }
+            
         }
     }
 }
