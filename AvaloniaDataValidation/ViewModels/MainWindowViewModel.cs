b596ba84a4eddbb931e9270c7e94dfb61cc73309
bodong <bodong@tencent.com>
2023-04-14 09:59:25 +0800

    data validation.

--- AvaloniaDataValidation/ViewModels/MainWindowViewModel.cs
+++ AvaloniaDataValidation/ViewModels/MainWindowViewModel.cs
@@ -1,4 +1,5 @@
 ﻿using ReactiveUI;
+using System;
 using System.ComponentModel.DataAnnotations;
 
 namespace AvaloniaDataValidation.ViewModels
@@ -12,6 +13,47 @@ namespace AvaloniaDataValidation.ViewModels
         {
             get => EmailCore;
             set => this.RaiseAndSetIfChanged(ref EmailCore, value);
+        }
+
+        public string[] CombItems => new string[]
+        {
+            "C",
+            "C++",
+            "C#",
+        };
+
+        string ProgramLanguageCore = "C";
+
+        [LaungageValidation]
+        public string ProgramLanguage
+        {
+            get => ProgramLanguageCore;
+            set 
+            {
+                // can't use this
+//                 if(value == "C#")
+//                 {
+//                     throw new ArgumentException($"{nameof(ProgramLanguage)} can not use C#");
+//                 }
+
+                this.RaiseAndSetIfChanged(ref ProgramLanguageCore, value);
+            }
+        }
+    }
+
+    internal class LaungageValidationAttribute : ValidationAttribute
+    {
+        protected override ValidationResult? IsValid(object? value, ValidationContext validationContext)
+        {
+            if(value is string lan)
+            {
+                if (lan == "C#")
+                {
+                    return new ValidationResult($"[{validationContext.DisplayName}] can not be C#");
+                }
+            }
+
+            return ValidationResult.Success;
         }
     }
 }
